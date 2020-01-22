1210236ae430d117c71846db030097380fa62cf7
WesIngwersen <ingwersen.wesley@epa.gov>
2020-01-22 11:23:10 -0500

    -Major revision -change validate to validate_EEIO_model to be consistent -add dq param to calculate -clean up write function

--- useeiopy/__init__.py
+++ useeiopy/__init__.py
@@ -1,10 +1,12 @@
 """
-A wrapper for the IO-Model-Builder for use with USEEIO family models
+A wrapper for the IO-Model-Builder for use with USEEIO family models.
+Models are built from component pieces created by useeior
+Functions are analagous to useeior with PEP names conventions
+
 """
 import os
 import useeiopy.build as build
 import useeiopy.calculate as calculate
-import useeiopy.ValidateModel as ValidateModel
 import useeiopy.check as check
 from useeiopy.common import modelbuildpath, log
 
@@ -19,29 +21,66 @@ def build_EEIO_model(inputmodelname):
 
     #status code
     #0  = all files present .. proceed with assembly
-    #-2 = missing other files ...abort
+    #-2 = missing files ...abort
     case = check.look_for_model_files(modelname,modelpath)
 
     if case == 0:
         log.info("Assembling model...")
         model = build.make(modelname, modelpath)
+        model = model.add_matrices()
         return(model)
 
-def validate(model):
-    ValidateModel.validate(model)
 
-def calculate_EEIO_model(model,year=2007,location='US',demandtype='Consumption',perspective='DIRECT'):
-    result = calculate.calculate_EEIO_model(model,
-                             year,
-                             location,
-                             demandtype,
-                             perspective)
+def validate_EEIO_model(model):
+    """
+    Validates EEIO model
+    :param model: A useeiopy Model object
+    :return: None, writes validation out to file
+    """
+    import iomb.validation as v
+    valid = v.validate(model.model)
+    valid.display_count = -1
+    f = open(model.path + model.name + '_validate.txt', 'w')
+    f.write(valid.__str__())
+    f.close()
+
+def calculate_EEIO_model(model,
+                         year=2007,
+                         location='US',
+                         demandtype='Consumption',
+                         perspective='DIRECT',
+                         include_data_quality=False):
+    """
+    Calculates LCI, LCIA and optionally DQI results for an identified demand vector
+    present in the demand df identified by year, location and demandtype
+    :param model: A useeiopy Model object
+    :param year: int, year of demand
+    :param location: string, location acronym of demand
+    :param demandtype: string, either 'production' or 'consumption'
+    :param perspective: string, either 'DIRECT' or 'INDIRECT'
+    :return: A useeiopy Result object with LCI, LCIA and LCIA dataframes and metadata
+    """
+
+    result = calculate.calculate_EEIO_model(model, year, location,
+                                            demandtype, perspective, include_data_quality)
     return result
 
 def write_EEIO_result(result):
+    """
+    Writes out LCI, LCIA, and optionally DQI matrices to csv
+    :param result: a useeiopy Result object
+    :return: None
+    """
     resultsfolder = result.path + "results/"
     if os.path.exists(resultsfolder) is False:
         os.mkdir(resultsfolder)
-    result.LCI.to_csv(resultsfolder+result.modelname+"_"+ str(result.year)+"_"+result.location+"_"+result.demandtype+"_"+result.perspective+"_"+"LCI.csv")
-    result.LCIA.to_csv(resultsfolder+result.modelname+"_"+ str(result.year)+"_"+result.location+"_"+result.demandtype+"_"+result.perspective+"_"+"LCIA.csv")
+    result_pre = str(resultsfolder + result.modelname + "_" +
+                 str(result.year) + "_" +
+                 result.location + "_" +
+                 result.demandtype + "_" +
+                 result.perspective + "_")
+    result.LCI.to_csv(result_pre + "LCI.csv")
+    result.LCIA.to_csv(result_pre + "LCIA.csv")
+    if result.DQI is not None:
+        result.DQI.to_csv(result_pre + "DQI.csv")
     log.info("Wrote results files to " + resultsfolder)
