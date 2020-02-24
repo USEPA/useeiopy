4253044adcb1622baf4a6a113e0e471528a78d6f
WesIngwersen <ingwersen.wesley@epa.gov>
2020-02-24 17:28:26 -0500

    Update version number to distinguish updated iomb requirement

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='useeiopy',
-    version='0.0.1',
+    version='0.0.2',
     packages=['useeiopy'],
     package_data={'useeiopy': ["data/*.*"]},
     url='https://github.com/USEPA/useeiopy',
